b842349eb3a6610745c80496eea6e067108cff22
spin~ <65109978+spinfal@users.noreply.github.com>
2021-09-08 05:53:49 +0000

    changed for loop (just in case /shrug)

--- commands/search.js
+++ commands/search.js
@@ -2,7 +2,7 @@ const { Discord, MessageEmbed } = require('discord.js');
 const  googleIt = require('google-it')
 
 module.exports.run = async (client, message, args) => {
-  const searchLimit = (isNaN(args[0])) ? 10 : parseInt(args.shift());
+  let searchLimit = (isNaN(args[0])) ? 10 : parseInt(args.shift());
   if (searchLimit > 30) {
     const errorEmbed = new MessageEmbed()
       .setDescription(`The highest limit you can set is 30.`);
@@ -21,14 +21,14 @@ module.exports.run = async (client, message, args) => {
     }
     let searchResults = [];
   googleIt({'query': searchTerm, 'limit': searchLimit, 'disableConsole': true}).then(results => {
-    for (let i = 0; i < results.length; i++) {
+    for (let i = 0; i !== results.length; i++) {
       searchResults.push(`[**__${results[i].title}__**](${results[i].link})`);
       searchResults.push(`${results[i].snippet}\n`);
     }
     const resultEmbed = new MessageEmbed()
       .setTitle(`Search results for \`${searchTerm}\``)
       .setDescription(searchResults.join('\n'))
-      .setFooter(`Found ${results.length} results | Provided by Google`);
+      .setFooter(`Found ${results.length} ${(results.length > 1) ? "results" : "result"} | Provided by Google`);
     return m.edit(resultEmbed);
   }).catch(e => {
     const errorEmbed = new MessageEmbed()
